39dac3d55344f33d6860f20869d0786fb4022375
roshankarkera <roshankarkera33@gmail.com>
2025-08-14 07:40:51 +0530

    Testinmg 22

--- src/testing2.java
+++ src/testing2.java
@@ -4,7 +4,7 @@ public class testing2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testing 2");
+		System.out.println("Testing 22");
 
 	}
 
